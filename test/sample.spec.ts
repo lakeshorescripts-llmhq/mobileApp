23307f0a9bc4e7387a15f054c03a9c50d406ac8e
Dereck Queza <lakeshorescripts@gmail.com>
2025-09-24 20:16:53 -0700

    cleanup: remove example test files

--- test/sample.spec.ts
+++
@@ -1,6 +0,0 @@
-describe('Sample Appium Test', () => {
-  it('should launch the app', async () => {
-    const appTitle = await $('~AppTitle');
-    await expect(appTitle).toBeDisplayed();
-  });
-});
